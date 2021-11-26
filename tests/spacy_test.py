9c712429b4ffb38f44fe405bbff24e87c24b23be
fatimaaaaaaa <44090391+fatimaaaaaaa@users.noreply.github.com>
2021-11-26 14:13:31 +0100

    Update spacy_test.py

--- tests/spacy_test.py
+++ tests/spacy_test.py
@@ -1,15 +1,48 @@
+import os
+from spacy.kb import KnowledgeBase
 import spacy  # version 3.0.6'
+from pathlib import Path
+import csv
 
-# initialize language model
+
+def load_entities():
+    # distributed alongside this notebook
+    entities_loc = Path.cwd().parent / "assignment\data\sample_annotations.tsv"
+
+    names = dict()
+    wikilinks = dict()
+    with entities_loc.open("r", encoding="utf8") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter="\t")
+        for row in csvreader:
+            qid = row[0]
+            name = row[1]
+            wikilink = row[2]
+            names[qid] = name
+            wikilinks[qid] = wikilink
+    return names, wikilinks
+
+
+name_dict, wikilink_dict = load_entities()
+for QID in name_dict.keys():
+    print(f"{QID}, name={name_dict[QID]}, wikilink={wikilink_dict[QID]}")
+
+
 nlp = spacy.load("en_core_web_md")
-# add pipeline (declared through entry_points in setup.py)
-entity_linker = nlp.add_pipe("entity_linker", last=True)
-entity_linker.set_kb('data/sample.warc.gz')
+kb = KnowledgeBase(vocab=nlp.vocab, entity_vector_length=300)
 
-doc = nlp("I watched the Pirates of the Caribbean last silvester")
+for qid, wikilink in wikilink_dict.items():
+    wikilink_doc = nlp(wikilink)
+    wikilink_enc = wikilink_doc.vector
+    kb.add_entity(entity=qid, entity_vector=wikilink_enc, freq=342)
 
-# returns all entities in the whole document
-all_linked_entities = doc._.linkedEntities
-# iterates over sentences and prints linked entities
-for sent in doc.sents:
-    sent._.linkedEntities.pretty_print()
+for qid, name in name_dict.items():
+    kb.add_alias(alias=name, entities=[qid], probabilities=[1])
+
+print(f"Entities in the KB: {kb.get_entity_strings()}")
+print(f"Aliases in the KB: {kb.get_alias_strings()}")
+
+
+# output_dir = Path.cwd().parent / "assignment"
+# if not os.path.exists(output_dir):
+#     os.mkdir(output_dir)
+# kb.dump(output_dir / "knowledgebase")
