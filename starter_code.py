ad564d10ab95da7518ca9cb29cc6107e33dbc973
vuwdps <94533370+vuwdps@users.noreply.github.com>
2021-11-26 16:34:35 +0100

    Update starter_code.py

--- starter_code.py
+++ starter_code.py
@@ -1,7 +1,7 @@
 import gzip
 import requests
 import json
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, ElasticsearchException
 from bs4 import BeautifulSoup
 import spacy
 
@@ -27,35 +27,55 @@ def find_labels(payload):
     print(key)
     namedEntity_list = {}
     extractedText = BeautifulSoup(payload, 'html.parser')
-    extractedText = extractedText.get_text().replace("\n", " ")
+    
+    extractedText = extractedText.get_text().replace("\n", " ") 
+
     nlp = spacy.load('en_core_web_sm')
     doc = nlp(extractedText)
-    
+        
     false_entities = ("DATE", "TIME", "PERCENT", "MONEY", "QUANTITY", "ORDINAL", "CARDINAL")
     for i in doc.ents:
-        if i.label_ not in false_entities:
-            namedEntity_list[i.text] = i.label_
-    print(namedEntity_list)
-    # response = requests.get(dbapi + "candidates?text="+extractedText)
-    # print('candidate text', response.content)
-    # es_results = search("Cola")
-    # print(es_results)
+            if i.label_ not in false_entities:
+                namedEntity_list[i.text] = i.label_
+
+    for keyent in namedEntity_list:
+            es_results=search(keyent)
+            result = {}
+            if es_results:
+                for hit in es_results['hits']['hits']:
+                    if 'schema_name' in hit['_source']:
+                        label = hit['_source']['schema_name']
+                        id = hit['_id']
+                        score = hit['_score']
+
+                        if result.get(label):
+                            change_score = max(result[label]['score'], score)
+                            result[label]['score'] = change_score
+
+                        result[label]= ({
+                                'id': id,
+                                'score':score,
+                                'rank': 0
+                            })
+            print(result)            
+            
+        # response = requests.get(dbapi + "candidates?text="+extractedText)
+        # print('candidate text', response.content)
+        # es_results = search("Cola")
+        # print(es_results)
     cheats = dict((line.split('\t', 2) for line in open('data/sample-labels-cheat.txt').read().splitlines()))
     for label, wikidata_id in cheats.items():
-        if key and (label in payload):
-            yield key, label, wikidata_id
+            if key and (label in payload):
+                yield key, label, wikidata_id
 
 def search(query):
     e = Elasticsearch(['http://fs0.das5.cs.vu.nl:10010'])
     p = { "query" : { "query_string" : { "query" : query }}}
-    response = e.search(index="wikidata_en", body=json.dumps(p))
-    id_labels = {}
-    if response:
-        for hit in response['hits']['hits']:
-            label = hit['_source']['schema_name']
-            id = hit['_id']
-            id_labels.setdefault(id, set()).add(label)
-    return id_labels
+    try:
+        response = e.search(index="wikidata_en", body=json.dumps(p))
+        return response
+    except ElasticsearchException as e:
+        pass
 
 def split_records(stream):
     payload = ''
