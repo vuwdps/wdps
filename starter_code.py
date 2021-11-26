c1f27f3f0bc522af89b864d5e1d79121ab7d1546
vuwdps <94533370+vuwdps@users.noreply.github.com>
2021-11-26 21:38:09 +0100

    Update starter_code.py

--- starter_code.py
+++ starter_code.py
@@ -4,7 +4,32 @@ import json
 from elasticsearch import Elasticsearch, ElasticsearchException
 from bs4 import BeautifulSoup
 import spacy
+import urllib
+import math
 
+
+
+HOST = "http://fs0.das5.cs.vu.nl:10011/sparql"
+
+def sparqlQuery(query, format="application/json"):
+    resp = requests.get(HOST + "?" + urllib.parse.urlencode({
+        "default-graph": "",
+        "should-sponge": "soft",
+        "query": query,
+        "debug": "on",
+        "timeout": "",
+        "format": format,
+        "save": "display",
+        "fname": ""
+    }))
+
+    return json.loads(resp.content.decode("utf-8"))
+
+
+
+
+
+a=1
 dbapi = "https://api.dbpedia-spotlight.org/en/"
 KEYNAME = "WARC-TREC-ID"
 
@@ -24,24 +49,26 @@ def find_labels(payload):
             key = line.split(': ')[1]
             break
 
-    print(key)
     namedEntity_list = {}
     extractedText = BeautifulSoup(payload, 'html.parser')
     
-    extractedText = extractedText.get_text().replace("\n", " ") 
-
+    extractedText = extractedText.get_text().replace("\n", " ")
     nlp = spacy.load('en_core_web_sm')
     doc = nlp(extractedText)
-        
     false_entities = ("DATE", "TIME", "PERCENT", "MONEY", "QUANTITY", "ORDINAL", "CARDINAL")
-    false_labels = ("WARC-Type","WARC-Date","clueweb12","WARC-IP-Address","WARC-Target-URI","Content-Type","GMT Cache-Control","§","ORG","OpenSSL/0.9.8e")
+    false_labels = ("WARC-Type","WARC-Date","clueweb12","WARC-IP-Address","WARC-Target-URI","Content-Type","GMT Cache-Control","§","ORG","OpenSSL/0.9.8e","ACCEPT",\
+        "WARC Format 1.0 specification","WARC Format 1.1 specification","WARC 1.1","WARC","WARC 1.0","WARC-Payload-Digest:","Content-Length","GMT Server",\
+        "Apache X-Powered-By","sha1","Accept-Encoding","WARC-Filename","WARC-File-Length","WARC-Data-Type","WARC-Record-ID","WARC File Format","WARC","WARC bands","WARC Format 1.0 specification")
+    
     for i in doc.ents:
             if i.label_ not in false_entities and i.text not in false_labels:
                 namedEntity_list[i.text] = i.label_
     
+    result = {}
+    print(namedEntity_list)
     for keyent in namedEntity_list:
             es_results=search(keyent)
-            result = {}
+            print(es_results)
             if es_results:
                 for hit in es_results['hits']['hits']:
                     if 'schema_name' in hit['_source']:
@@ -58,15 +85,27 @@ def find_labels(payload):
                                 'score':score,
                                 'rank': 0
                             })
+
+                        query = "select distinct * where {"+id+" ?pred ?obj.}"
+                        sparqlQuerydata = sparqlQuery(query)
+                        if sparqlQuerydata['results']:
+                            bindings = sparqlQuerydata['bindings']
+                            result[label]['rank'] = math.log(len(bindings)) * result[label]['score']
+
+            if result.items is not None:
+                result = sorted(result.items(), key=lambda i: (i[1]['rank']), reverse=True)
+
+                        # if sparqlQuerydata:
+                        #     n = int()
+    
                      
-            print(result)
+
         # response = requests.get(dbapi + "candidates?text="+extractedText)
         # print('candidate text', response.content)
         # es_results = search("Cola")
         # print(es_results)
-    cheats = dict((line.split('\t', 2) for line in open('data/sample-labels-cheat.txt').read().splitlines()))
-    for label, wikidata_id in cheats.items():
-            if key and (label in payload):
+    
+    for label, id in result.items():
                 yield key, label, wikidata_id
 
 def search(query):
